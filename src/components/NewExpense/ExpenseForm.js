7c3bd5f1723d33cfdc5feccadcc1fe9c0df8225d
Abhinav Sawarkar <abhisawarkar85@gmail.com>
2021-09-28 16:39:12 +0530

    bug fix in ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -58,7 +58,7 @@ function ExpenseForm(props) {
     e.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     // console.log(expenseData);
